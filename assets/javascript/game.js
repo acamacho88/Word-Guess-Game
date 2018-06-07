b85d3fdfa6b5eeed37f262600005dd3358163d37
Alex Camacho <alex.c@WalkMe.local>
2018-06-06 23:16:30 -0700

    Finished game, added images, audio, and background color customizations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,59 @@
-var bandsList = [
-    "Metallica",
-    "Ozzy Osbourne",
-    "Judas Priest",
-    "Iron Maiden",
-    "Megadeth",
-    "Pantera",
-    "Slayer",
-    "Rainbow",
-    "Motorhead"
-];
+var bandsList = {
+    "Metallica":
+    {
+     "image":"assets/images/metallica.jpg",
+     "audio":"assets/audio/05 Disposable Heroes.mp3",
+     "color":"#3f0d0c"
+    },
+    "Ozzy Osbourne":
+    {
+     "image":"assets/images/ozzy.jpg",
+     "audio":"assets/audio/06 - Mr. Crowley.mp3",
+     "color":"#555347"
+    },
+    "Judas Priest":
+    {
+     "image":"assets/images/judaspriest.jpg",
+     "audio":"assets/audio/07 - Screaming For Vengeance.mp3",
+     "color":"#f1c43e"
+    },
+    "Iron Maiden":
+    {
+     "image":"assets/images/ironmaiden.jpg",
+     "audio":"assets/audio/07 Killers.m4a",
+     "color":"#211a22"
+    },
+    "Megadeth":
+    {
+     "image":"assets/images/megadeth.jpg",
+     "audio":"assets/audio/01 Holy Wars...The Punishment Due.m4a",
+     "color":"#232347"
+    },
+    "Pantera":
+    {
+     "image":"assets/images/pantera.jpg",
+     "audio":"assets/audio/01 Cowboys from Hell.m4a",
+     "color":"#654336"
+    },
+    "Slayer":
+    {
+     "image":"assets/images/slayer.jpg",
+     "audio":"assets/audio/01 Angel of Death.m4a",
+     "color":"#2a1a0b"
+    },
+    "Rainbow":
+    {
+     "image":"assets/images/rainbow.jpg",
+     "audio":"assets/audio/03 Starstruck.m4a",
+     "color":"#321510"
+    },
+    "Motorhead":
+    {
+     "image":"assets/images/motorhead.jpg",
+     "audio":"assets/audio/11 Motorhead.m4a",
+     "color":"black"
+    }
+};
 
 var nWins = 0;
 
@@ -25,6 +70,12 @@ var guessSpace = document.getElementById('guess-spaces');
 var numberWins = document.getElementById('nwins');
 
 var numberGuesses = document.getElementById('nguesses');
+
+var lettersGuessed = document.getElementById('letters-guessed');
+
+var winningImg = document.getElementById('winning-img');
+
+var audioElement = document.createElement("audio");
 
 function isAlpha(letter) {
     for (var i = 0; i < alphabet.length; i++) {
@@ -63,17 +114,41 @@ function updateStatusString() {
         guessSpace.textContent = statusString;
     } else {
         nWins++;
+        winningImg.setAttribute("src",bandsList[currWord]["image"]);
+        winningImg.setAttribute("height","320");
+        winningImg.setAttribute("width","320");
+        winningImg.setAttribute("height","300");
+        document.body.style.backgroundColor = bandsList[currWord]["color"];
+        audioElement.setAttribute("src", bandsList[currWord]["audio"]);
+        audioElement.play();
         newBand();
     }
 }
 
+function updateUsedLetters() {
+    var usedString = '';
+    for (var i = 0; i < badGuesses.length; i++) {
+        if (i == 0) {
+            usedString += badGuesses[0];
+        } else {
+            usedString += ', ' + badGuesses[i];
+        }
+    }
+    lettersGuessed.textContent = usedString;
+}
+
 function newBand() {
-    currWord = bandsList[Math.floor(Math.random() * bandsList.length)];
+    var namesList = Object.keys(bandsList);
+    var prevWord = currWord;
+    while(prevWord == currWord) {
+        currWord = namesList[Math.floor(Math.random() * namesList.length)];
+    }
     goodGuesses = [];
     badGuesses = [];
     numberWins.textContent = nWins;
     numberGuesses.textContent = 13;
     updateStatusString();
+    updateUsedLetters();
 }
 
 function isNewGuess(letter) {
@@ -105,6 +180,7 @@ function validGuess(letter) {
         if (badGuesses.length == 13) {
             newBand();
         }
+        updateUsedLetters();
         numberGuesses.textContent = 13 - badGuesses.length;
     }
 }
